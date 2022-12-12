bd07eac158828bb4ac255900b94faf87f822a815
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-12-12 10:21:27 +0000

    :)

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,43 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
+
 console.log("Welcome to search-movies-cli!");
+
+const readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+
+
+
+
+ 
+async function searchForMovieInDB(){
+    
+    let programmeStillRunning = true
+
+    const text = 
+    `SELECT name, date, runtime, revenue, vote_average, votes_count 
+    FROM movies
+    WHERE UPPER(name) LIKE UPPER($1)
+    AND kind = 'movie' 
+    ORDER BY name 
+    DESC LIMIT 10`
+
+    while (programmeStillRunning === true) {
+        let movieSearch = readlineSync.question('Search for a movie or press "q" to quit! ');
+        if(movieSearch === 'q'){
+            programmeStillRunning = false
+        } else{
+            const value = [`%${movieSearch}%`]
+            const client = new Client({ database: 'omdb' });
+            await client.connect();
+            const res = await client.query(text, value);
+            console.table(res.rows);
+            await client.end();
+        }
+        
+    }
+}
+
+searchForMovieInDB()
