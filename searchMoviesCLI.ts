7089102c1f2822cdea73b4e0cfe43f9b1f616239
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-12-12 10:35:27 +0000

    Write movieDBProgramme function

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -12,13 +12,30 @@ const readlineSync = require('readline-sync');
 // Wait for user's response.
 
 
+async function movieDBProgramme() {
+    let programmeStillRunning = true;
+    let actionSelection = readlineSync.question('Welcome to search-movies-cli!/n[1] Search/n[2] See Favourites/n[3] Quit/nChoose an action! [1, 2, 3]: ');
 
+    while(programmeStillRunning){
+        if(actionSelection===1){
+            searchForMovieInDB()
+        } else if (actionSelection===2){
+            showFavourites()
+        } else if(actionSelection===3){
+            programmeStillRunning=false
+        } else{
+            actionSelection = readlineSync.question('Not a valid selection!/n[1] Search/n[2] See Favourites/n[3] Quit/nChoose an action! [1, 2, 3]: ')
+        }
+    }
+}
+
+async function showFavourites() {
+    
+}
 
  
 async function searchForMovieInDB(){
     
-    let programmeStillRunning = true
-
     const text = 
     `SELECT name, date, runtime, revenue, vote_average, votes_count 
     FROM movies
@@ -27,20 +44,14 @@ async function searchForMovieInDB(){
     ORDER BY name 
     DESC LIMIT 10`
 
-    while (programmeStillRunning === true) {
-        let movieSearch = readlineSync.question('Search for a movie or press "q" to quit! ');
-        if(movieSearch === 'q'){
-            programmeStillRunning = false
-        } else{
-            const value = [`%${movieSearch}%`]
-            const client = new Client({ database: 'omdb' });
-            await client.connect();
-            const res = await client.query(text, value);
-            console.table(res.rows);
-            await client.end();
-        }
-        
+    let movieSearch = readlineSync.question('Search for a movie! ');
+    const value = [`%${movieSearch}%`]
+    const client = new Client({ database: 'omdb' });
+    await client.connect();
+    const res = await client.query(text, value);
+    console.table(res.rows);
+    await client.end();
     }
-}
+
 
 searchForMovieInDB()
