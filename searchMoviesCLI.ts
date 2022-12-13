1ca22a4edca0ddfb08c5fd4a2587cd33f9d0301d
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-12-13 11:03:11 +0000

    :D

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -14,17 +14,16 @@ const readlineSync = require('readline-sync');
 
 async function movieDBProgramme() {
     let programmeStillRunning = true;
-    let actionSelection = readlineSync.question(`Welcome to search-movies-cli!/n[1] Search/n[2] See Favourites/n[3] Quit/nChoose an action! [1, 2, 3]: `);
-
     while(programmeStillRunning){
+        let actionSelection:string = readlineSync.question(`Welcome to search-movies-cli!\n[1] Search\n[2] See Favourites\n[3] Quit\nChoose an action! [1, 2, 3]: `);
         if(actionSelection==='1'){
             await searchForMovieInDB()
-        } else if (actionSelection===2){
+        } else if (actionSelection==='2'){
             await showFavourites()
-        } else if(actionSelection===3){
+        } else if(actionSelection==='3'){
             programmeStillRunning=false
         } else{
-            actionSelection = readlineSync.question('Not a valid selection!/n[1] Search/n[2] See Favourites/n[3] Quit/nChoose an action! [1, 2, 3]: ')
+            actionSelection = readlineSync.question('Not a valid selection!\n[1] Search\n[2] See Favourites\n[3] Quit\nChoose an action! [1, 2, 3]: ')
         }
     }
 }
@@ -39,37 +38,45 @@ async function showFavourites() {
 
  
 async function searchForMovieInDB(){
+    let stillSearching = true
+    while(stillSearching){
+        const searchText = 
+        `SELECT id, name, date_part('year', date) AS year, runtime, revenue, vote_average, votes_count 
+        FROM movies
+        WHERE UPPER(name) LIKE UPPER($1)
+        AND kind = 'movie' 
+        ORDER BY name 
+        DESC LIMIT 10`;
+
+        const favouriteText = 
+        `INSERT INTO favourites (movie_id, name, year, runtime)
+        VALUES ($1, $2, $3, $4)`
+
+        let movieSearch = readlineSync.question('Search for a movie! ');
+        const searchValue = [`%${movieSearch}%`]
+        const client = new Client({ database: 'omdb' });
+        await client.connect();
+        const res = await client.query(searchText, searchValue);
+        console.log(`Search term: ${movieSearch}`)
+        console.table(res.rows);
+        for(let i=0; i<res.rows.length; i++){
+            console.log(`[${i+1}] ${res.rows[i].name}`)
+        }
+        console.log(`[0] CANCEL`)
+        let responseString: number = Number(readlineSync.question('Choose a movie row number to favourite: '))
+        let favSelection = res.rows[Number(responseString) -1]
+        if(responseString > 0 && responseString < res.rows.length){
+            console.log(`Saving favourite movie: ${favSelection.name}`)
+            let faveValues = [favSelection.id, `${favSelection.name}`, favSelection.year, favSelection.runtime]
+            await client.query(favouriteText, faveValues)
+            await client.end();
+        } else if (responseString === 0) {
+            stillSearching = false
+        }
+    }
     
-    const searchText = 
-    `SELECT id, name, date_part('year', date) AS year, runtime, revenue, vote_average, votes_count 
-    FROM movies
-    WHERE UPPER(name) LIKE UPPER($1)
-    AND kind = 'movie' 
-    ORDER BY name 
-    DESC LIMIT 10`;
 
-    const favouriteText = 
-    `INSERT INTO favourites (movie_id, name, year, runtime)
-    VALUES ($1, `
-
-    let movieSearch = readlineSync.question('Search for a movie! ');
-    const searchValue = [`%${movieSearch}%`]
-    const client = new Client({ database: 'omdb' });
-    await client.connect();
-    const res = await client.query(searchText, searchValue);
-    console.log(`Search term: ${movieSearch}`)
-    console.table(res.rows);
-    for(let i=0; i<res.rows.length; i++){
-        console.log(`[${i+1}] ${res.rows[i].name}`)
-    }
-    console.log(`[0] CANCEL`)
-    let favSelection = res.rows[readlineSync.question('Choose a movie row number to favourite: ') -1]
-    console.log(`Saving favourite movie: ${favSelection.name}`)
-    const insertQuery = `INSERT INTO favourites (movie_id, name, year, runtime) VALUES (${favSelection.id}, ${favSelection.name}, ${favSelection.year}, ${favSelection.runtime})`
-    console.log(insertQuery);
-    await client.query(insertQuery)
-    await client.end();
-    }
+}
 
 
 //searchForMovieInDB()
